d96fc4c13c6d5325061f9d908926a88355798d7f
luisfbello <luisfbellogonzalez@gmail.com>
2020-08-10 21:57:30 -0500

    creacion aplicacion mvc

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AplicacionWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
